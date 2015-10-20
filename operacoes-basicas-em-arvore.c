ba9dfbff37673b091786d21a9df4150679a0334b
cristhianmafalda <cristhianmafalda@gmail.com>
2015-10-20 19:31:33 -0200

    Versão 0.5
    Menu ok,
    insercao ok
    busca ok
    falta remoção

--- operacoes-basicas-em-arvore.c
+++ operacoes-basicas-em-arvore.c
@@ -39,23 +39,19 @@ void add(struct arvore*no, int chave){			//add é a função que adiciona um valor 
 
 }
 
-void localiza(struct arvore*no, int busca){
+struct arvore*search(struct arvore*no, int busca){
 	
-	int i=0;
-	while(no!=NULL){
-		if (no->key==busca){
-			i=1;
-		}
-		else if (busca>no->key){
-			localiza(no->dir,busca);
-		}
-		else localiza(no->esq,busca);
+	if(no==NULL){
+		return(NULL);
 	}
-	if(i==0){
-		printf("\nO valor buscado n%co existe\n",198);
+	if(busca == no->key){
+		return(no);
 	}
-	else {
-		printf("\nO valor buscado existe\n");
+	if(busca>no->key){
+		return(search(no->dir,busca));
+	}
+	if(busca<no->key){
+		return(search(no->esq,busca));
 	}
 }
 
@@ -93,6 +89,26 @@ void labelledbracketing(struct arvore*no){
 	printf("]");
 }
 
+int menu(){
+	
+		printf("\nDigite o n%cmero da op%c%co que deseja realizar com sua %crvore:",163,135,198,160);
+		printf("\nOp%c%co 1: Inser%c%co",135,198,135,198);
+		printf("\nOp%c%co 2: Remo%c%co",135,198,135,198);
+		printf("\nOp%c%co 3: Busca",135,198);
+		printf("\nOp%c%co 4: Impress%co Pr%c Ordem",135,198,198,130);
+		printf("\nOp%c%co 5: Impress%co Em Ordem",135,198,198);
+		printf("\nOp%c%co 6: Impress%co P%cs Ordem",135,198,198,162);
+		printf("\nOp%c%co 7: Impress%co Labelled Bracketing",135,198,198);
+		
+		printf("\nOp%c%co escolhida: ",135,198);
+		int opcao;
+		scanf("%d",&opcao);
+		printf("\n");
+		
+		return(opcao);
+	
+}
+
 void main () {
 	
 	printf("Digite a quantidade inicial de valores a ser digitado.\nEm seguida, insira os valores.\n\n");
@@ -112,62 +128,64 @@ void main () {
 			add(raiz,chave);
 		}
 		
-		printf("\nDigite o n%cmero da op%c%co que deseja realizar com sua %crvore:",163,135,198,160);
-		printf("\nOp%c%co 1: Inser%c%co",135,198,135,198);
-		printf("\nOp%c%co 2: Remo%c%co",135,198,135,198);
-		printf("\nOp%c%co 3: Busca",135,198);
-		printf("\nOp%c%co 4: Impress%co Pr%c Ordem",135,198,198,130);
-		printf("\nOp%c%co 5: Impress%co Em Ordem",135,198,198);
-		printf("\nOp%c%co 6: Impress%co P%cs Ordem",135,198,198,162);
-		printf("\nOp%c%co 7: Impress%co Labelled Bracketing",135,198,198);
-		printf("\nOp%c%co 0: Sair\n",135,198);
-		
-		printf("\nOp%c%co escolhida: ",135,198);
-		int opcao;
-		scanf("%d",&opcao);
-		printf("\n");
-		
-		if (opcao == 1){
-			printf("Digite a quantidade de valores a ser inserido.\nEm seguida, insira os valores.\n\n");
-			scanf("%d",&n);
-			for(i=0;i<n;i++){
-				scanf("%d",&chave);
-				add(raiz,chave);
+		int j=1;	
+		while(j==1){
+			
+			int opcao;
+			opcao=menu();
+			
+			if (opcao == 1){
+				printf("Digite a quantidade de valores a ser inserido.\nEm seguida, insira os valores.\n\n");
+				scanf("%d",&n);
+				for(i=0;i<n;i++){
+					scanf("%d",&chave);
+					add(raiz,chave);
+				}
 			}
-		}
-		
-		if (opcao == 2){
 			
-		}
-		
-		if (opcao == 3){
-			printf("\nDigite o valor buscado: ");
-			scanf("%d",&chave);
-			localiza(raiz,chave);
-		}
-		
-		if (opcao == 4){
-			preordem(raiz);
-		}
-		
-		if (opcao == 5){
-			emordem(raiz);
-		}
-		
-		if (opcao == 6){
-			posordem(raiz);
-		}
-		
-		if (opcao == 7){
-			labelledbracketing(raiz);
-		}
-		
-		if (opcao == 0){
+			if (opcao == 2){
+				
+			}
 			
+			if (opcao == 3){
+				printf("\nDigite o valor buscado: ");
+				scanf("%d",&chave);
+				struct arvore *encontrado = search(raiz,chave);
+				if(encontrado!=NULL){
+					if(encontrado->key==chave){
+						printf("\nO valor %d exite nessa %crvore\n",chave,160);
+					}
+				}
+				else{
+					printf("\nO valor buscado n%co existe nessa %crvore\n",198,160);
+				}
+			}
+			
+			if (opcao == 4){
+				preordem(raiz);
+				printf("\n");
+			}
+			
+			if (opcao == 5){
+				emordem(raiz);
+				printf("\n");
+			}
+			
+			if (opcao == 6){
+				posordem(raiz);
+				printf("\n");
+			}
+			
+			if (opcao == 7){
+				labelledbracketing(raiz);
+				printf("\n");
+			}
+			
+			printf("\n\nDeseja continuar no programa %c    1-SIM  2-N%cO\n",63,199);
+			scanf("%d",&j);
 		}
-
+	printf("\nPROGRAMA ENCERRADO");
 	}
-	
 getch();	
 }
 
